4590672e45f304969fbb5277764c00c7904bc1dc
Rafael Bonini <rafaeldesconfie@gmail.com>
2020-08-18 19:00:01 -0300

    Creating 'RealmFeedStore' to be realm implementation for offline storage. Making 'RealmFeedStore' comfort to FeedStore protocol.

---
+++ FeedStoreChallenge/RealmFeedStore.swift
@@ -0,0 +1,26 @@
+//
+//  RealmFeedStore.swift
+//  FeedStoreChallenge
+//
+//  Created by Rafael Bonini on 8/18/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public class RealmFeedStore: FeedStore {
+    
+    public init() { }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        
+    }
+}
